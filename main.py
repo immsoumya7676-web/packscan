b6c8a8e04eaaacd202bf9a8c0666bd0893d459c2
immsoumya7676-web <immsoumya7676@gmail.com>
2026-08-08 09:23:51 +0530

    Fix Railway startup

--- main.py
+++ main.py
@@ -149,6 +149,3 @@ async def analyze(request: Request, file: UploadFile = File(...)):
 }
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
